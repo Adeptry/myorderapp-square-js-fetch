dc4961e7fbe816477492e39067015a687567892b
PLJNS <me@pljns.com>
2023-10-13 00:25:31 -0400

    2.4.2 initial commit

---
+++ src/apis/index.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AppConfigsApi';
+export * from './AuthenticationApi';
+export * from './CardsApi';
+export * from './CatalogsApi';
+export * from './CustomersApi';
+export * from './LocationsApi';
+export * from './MailApi';
+export * from './MerchantsApi';
+export * from './OrdersApi';
+export * from './UsersApi';
